622ce4a0e684641e04dd2f88f393d6c1139bbbed
Cache Cake <m.saihomi@gmail.com>
2013-12-30 16:43:56 +0800

    0.0.5

--- imageUpload/js/imageUpload_page.js
+++ imageUpload/js/imageUpload_page.js
@@ -12,7 +12,9 @@ getImg_box.addEventListener('dragleave', function(e) {
 getImg_box.addEventListener('dragover', function(e) {
     e.preventDefault();
 }, false);
-getImg_box.addEventListener('drop', function(e) {
+
+getImg_box.addEventListener('drop', function(e) { //拖拽放下，执行上传操作
+
     e.preventDefault();
     imgs = e.dataTransfer.files;
 
@@ -23,6 +25,8 @@ getImg_box.addEventListener('drop', function(e) {
         imgUrlInput.attr('value', src).show();
         WINURL.revokeObjectURL(src);
     }, 300);
+
+    postImg(imgs[0]); //调用上传函数
 
     $('#getImg_box').animate({
         'height': '465px',
@@ -37,6 +41,34 @@ getImg_box.addEventListener('drop', function(e) {
 
 }, false);
 
-imgUrlInput.on('focus', function() {
+
+function postImg(file) { //上传图片函数
+    var formdata = new FormData();
+    formdata.append('file', file);
+
+    var xhr = new XMLHttpRequest();
+    var upload = xhr.upload;
+
+    //上传进度
+    upload.addEventListener("progress", function(ev) {
+        if (ev.lengthComputable) {
+            console.log(ev.loaded / ev.total);
+        }
+    }, false);
+
+    //完成事件
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            //do somthing width your respose.
+            alert('success');
+        }
+    };
+
+    xhr.open('POST', url); //url 是表单的提交地址。
+    xhr.send(formdata);
+}
+
+
+imgUrlInput.on('focus', function() { //返回地址框focus后，自动全选
     //全选
 });
